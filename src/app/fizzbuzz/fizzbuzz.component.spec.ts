08cf46c397e42db2f1e812e516f4a702cb4e2bae
Chris Jansen <Chris Jansen>
2021-09-02 13:38:20 +0200

    pre refactoring

--- src/app/fizzbuzz/fizzbuzz.component.spec.ts
+++ src/app/fizzbuzz/fizzbuzz.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FizzbuzzComponent } from './fizzbuzz.component';
+import { By } from '@angular/platform-browser';
 
 describe('FizzbuzzComponent', () => {
   let component: FizzbuzzComponent;
@@ -22,4 +23,23 @@ describe('FizzbuzzComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it ('should have a start field', () => {
+    expect(
+      fixture.debugElement.query(By.css('input[name=start]')).nativeElement
+    ).toBeDefined();
+  })
+
+  it ('should have an end field', () => {
+    expect(
+      fixture.debugElement.query(By.css('input[name=end]')).nativeElement
+    ).toBeDefined();
+  })
+
+  it ('should have a submit button', () => {
+    expect(
+      fixture.debugElement.query(By.css('button[type=submit]')).nativeElement
+    ).toBeDefined();
+  })
+
 });
---
+++ src/app/fizzbuzz/requirements.md
@@ -0,0 +1,7 @@
+
+
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz
+
+
+neem de bovenstaande implementatie en pas die aan zodat de gebruiker 2 invoervelden kan vullen met een start getal en een eind getal. De implementatie 
